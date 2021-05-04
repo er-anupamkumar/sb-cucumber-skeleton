9d282ea4b9c915e176bac557a8046242d4272009
anupam <Lucknow@1307>
2021-05-04 21:14:43 +0200

    first commit

---
+++ src/main/java/com/anupamkumar/sbcucumberskeleton/SbCucumberSkeletonApplication.java
@@ -0,0 +1,13 @@
+package com.anupamkumar.sbcucumberskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCucumberSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbCucumberSkeletonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anupamkumar/sbcucumberskeleton/SbCucumberSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anupamkumar.sbcucumberskeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbCucumberSkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
